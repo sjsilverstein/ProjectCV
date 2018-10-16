bd2044e5a501a165cbc585923a90e3b73d3236a0
Stephen Silverstein <stephenjsilverstein@gmail.com>
2018-10-16 14:28:44 -0700

    Fixes
    Fixed Navbar and mobile scaling issues.

--- wwwroot/js/myJS.js
+++ wwwroot/js/myJS.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
+
     $('.carousel').carousel({        
         interval: 5000
+    });
+
+//Rotates img 360 degrees when moused over and out.
+    $("img.iconFlip").hover(function(){
+        $(this).removeClass('outIcon');
+        $(this).toggleClass("overIcon");
+        
+        
+    },function(){
+        $(this).toggleClass("overIcon");
+        $(this).addClass('outIcon');
+    });
+//Elevates box shadowing of cards to "move" the cards
+    $(".resume_body_card").hover(function(){
+        $(this).toggleClass('box-shadow-hover');
+        $(this).toggleClass('box-shadow-hover-deep');
+    },function(){
+        $(this).toggleClass('box-shadow-hover');
+        $(this).toggleClass('box-shadow-hover-deep');
     });
 })
